4ae4a094cec0cbe51bd47b1b03641f0ad873513b
WillianMendes <willianmendesbr@gmail.com>
2021-08-20 19:11:20 -0300

    feat: send token from recovery password (screen)

--- front/src/pages/RecoveryPassword.tsx
+++ front/src/pages/RecoveryPassword.tsx
@@ -1,26 +1,37 @@
 import React, { useEffect, useState } from 'react';
 
 import {
-  Button, Col, Form, Input, Layout, message, Row, Steps,
+  Button, Col, Form, Input, Layout, message, Modal, Row, Statistic, Steps,
 } from 'antd';
-import { MailOutlined, SendOutlined } from '@ant-design/icons';
-import { Content } from 'antd/lib/layout/layout';
+import {
+  CodeOutlined, ExclamationCircleOutlined, MailOutlined, SendOutlined,
+} from '@ant-design/icons';
 
+import { Content } from 'antd/lib/layout/layout';
 import AdminService from '../services/AdminService';
 
 const RecoveryPassword = () => {
   // Inputs
   const [email, setEmail] = useState<string>('');
 
+  // Buttons
+  const [returnButtonIsDisabled, setReturnButtonIsDisabled] = useState<boolean>(true);
+
   // Loadings
   const [loading, setLoading] = useState<boolean>(false);
+  const [countDownIsVisible, setCountDownIsVisible] = useState<boolean>(true);
 
   // Others
   const [step, setStep] = useState<number>(0);
 
+  // Variables
+  const deadline = Date.now() + 10 * 1000;
+
   // ANT Const
   const { Item } = Form;
   const { Step } = Steps;
+  const { Countdown } = Statistic;
+  const { warning } = Modal;
 
   async function validateStepZero() {
     const response = await AdminService.sendMailRecoveryPassword(email);
@@ -33,13 +44,33 @@ const RecoveryPassword = () => {
     }
   }
 
-  async function onFinishSuccess() {
+  async function validateStepOne(values: any) {
+    setCountDownIsVisible(false);
+    const { code } = values;
+    const response = await AdminService.verifyTokenRecoveryPassword(code, email);
+
+    if (response !== false && response !== true) {
+      if (response === undefined) message.error('Ops! Houve um erro no servidor, tente novamente mais tarde!');
+      if ('message' in response) message.error(response.message);
+    } else if (response) {
+      setStep(2);
+    } else if (!response) {
+      message.error('Ops! O token informado já expirou, solicite outro novamente!');
+    }
+
+    setCountDownIsVisible(true);
+  }
+
+  async function onFinishSuccess(values: any) {
     setLoading(true);
     switch (step) {
       case 0:
         await validateStepZero();
+        setReturnButtonIsDisabled(true);
+        setCountDownIsVisible(true);
         break;
       case 1:
+        await validateStepOne(values);
         break;
       case 2:
         break;
@@ -48,6 +79,47 @@ const RecoveryPassword = () => {
     }
     setLoading(false);
   }
+
+  const finishedCountDown = () => {
+    setReturnButtonIsDisabled(false);
+    setCountDownIsVisible(false);
+    warning({
+      title: 'Ainda não recebeu um e-mail com o código de confirmação?',
+      icon: <ExclamationCircleOutlined />,
+      content: (
+        <div className="mt-3">
+          <div>
+            <p>
+              Tente novamente.
+            </p>
+            <p>
+              Tenha certeza que o e-mail informado
+              <strong> é válido.</strong>
+            </p>
+            <p>
+              Verifique sua
+              <strong> caixa de entrada.</strong>
+            </p>
+            <p>
+              Verifique sua
+              <strong> caixa de spam.</strong>
+            </p>
+            <p>
+              Caso tenha dificuldade, entre em contato com o
+              <strong> administrador do sistema.</strong>
+            </p>
+          </div>
+        </div>
+      ),
+    });
+  };
+
+  const returnToStepZero = () => {
+    message
+      .info('Informe o mesmo e-mail que foi utilizado para se cadastrar e fazer login.', 10)
+      .then();
+    setStep(0);
+  };
 
   const getSubmitButton = (disabled: boolean = false) => (
     <Button type="primary" icon={<SendOutlined />} loading={loading} htmlType="submit" disabled={disabled}>
@@ -72,6 +144,36 @@ const RecoveryPassword = () => {
     </>
   );
 
+  const getStepOne = () => (
+    <>
+      <Item label="Código" name="code" wrapperCol={{ md: 24 }}>
+        <Input
+          type="number"
+          size="large"
+          placeholder="Insira o código que você recebeu por e-mail"
+          prefix={<CodeOutlined />}
+          disabled={loading}
+          required
+        />
+      </Item>
+      {getSubmitButton()}
+      <Button
+        type="link"
+        onClick={() => returnToStepZero()}
+        htmlType="button"
+        disabled={returnButtonIsDisabled}
+      >
+        Ainda não recebi o código
+      </Button>
+      <Col className="mt-3">
+        {
+          countDownIsVisible
+          && <Countdown title="Tente novamente em" value={deadline} onFinish={finishedCountDown} format="mm:ss" />
+        }
+      </Col>
+    </>
+  );
+
   // Umount States
   useEffect(() => () => {}, []);
 
@@ -89,6 +191,7 @@ const RecoveryPassword = () => {
           <Col span="24" className="mt-4">
             <Form name={`formStep${step}`} layout="vertical" onFinish={onFinishSuccess}>
               {step === 0 && getStepZero()}
+              {step === 1 && getStepOne()}
             </Form>
           </Col>
         </Row>
--- front/src/services/AdminService.ts
+++ front/src/services/AdminService.ts
@@ -2,6 +2,7 @@ import ErrorApi from '../types/ErrorApi';
 
 import {
   API_URL_ADMIN_SEND_MAIL_RECOVERY_PASSWORD as ADMIN_SEND_MAIL,
+  API_URL_ADMIN_VERIFY_TOKEN_RECOVERY_PASSWORD as ADMIN_VERIFY_TOKEN,
 } from '../const/API_POS';
 
 class AdminService {
@@ -12,6 +13,17 @@ class AdminService {
   static async sendMailRecoveryPassword(email: string): Promise<Boolean | ErrorApi> {
     try {
       const response = await fetch(`${ADMIN_SEND_MAIL}/${email}`);
+      return response.json();
+    } catch (error) {
+      this.errorApi.error = error;
+      return this.errorApi;
+    }
+  }
+
+  static async verifyTokenRecoveryPassword(token: string,
+    email: string): Promise<Boolean | ErrorApi> {
+    try {
+      const response = await fetch(`${ADMIN_VERIFY_TOKEN}/${token}/${email}`);
       return response.json();
     } catch (error) {
       this.errorApi.error = error;
