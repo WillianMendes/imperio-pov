8d4889e592dfcdc749bb131bcd28a33d1a4ca1b3
WillianMendes <willianmendesbr@gmail.com>
2021-08-26 13:20:14 -0300

    feat: page view products

--- front/src/pages/Products.tsx
+++ front/src/pages/Products.tsx
@@ -1,42 +1,35 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
 
 import {
   Button, PageHeader, Space, Table,
 } from 'antd';
-import { Link } from 'react-router-dom';
+
+import { CheckOutlined, CloseOutlined } from '@ant-design/icons';
+
+import Product from '../types/Product';
+import ProductService from '../services/ProductService';
+import { formatterNumber } from '../utils/MaskCurrency';
 
 function Products() {
   const { Column } = Table;
 
-  const data = [
-    {
-      key: '1',
-      name: 'Area Fina',
-      priceCoast: '12.75',
-      priceSale: '14.99',
-      stock: '50',
-      measurement: 'Metros Cubicos',
-      onDemand: 'Sim',
-    },
-    {
-      key: '2',
-      name: 'Area Grossa',
-      priceCoast: '10.24',
-      priceSale: '12.99',
-      stock: '90',
-      measurement: 'Metros Cubicos',
-      onDemand: 'Sim',
-    },
-    {
-      key: '3',
-      name: 'Parafuso 3/4',
-      priceCoast: '0.99',
-      priceSale: '1.29',
-      stock: '500',
-      measurement: 'Unidades',
-      onDemand: 'Não',
-    },
-  ];
+  // Data
+  const [products, setProducts] = useState<Product[]>();
+
+  // Loading
+  const [loading, setLoading] = useState<boolean>(false);
+
+  async function getProducts() {
+    setLoading(true);
+    const result = await ProductService.listAll();
+    if ('content' in result) setProducts(result.content);
+    setLoading(false);
+  }
+
+  useEffect(() => {
+    getProducts().then();
+  }, []);
 
   function getButtonNewProduct() {
     return (
@@ -52,27 +45,38 @@ function Products() {
     );
   }
 
-  return (
-    <div style={{ padding: 32 }}>
-      <PageHeader title="Produtos" extra={getButtonNewProduct()} />
-      <Table dataSource={data} style={{ marginTop: 32 }}>
+  function renderTable() {
+    return (
+      <Table dataSource={products} loading={loading} style={{ marginTop: 32 }}>
         <Column title="Nome" dataIndex="name" key="firstName" />
-        <Column title="Preço de Custo" dataIndex="priceCoast" key="priceCoast" />
-        <Column title="Preço de Venda" dataIndex="priceSale" key="priceSale" />
-        <Column title="Estoque" dataIndex="stock" key="stock" />
+        <Column title="Preço de Custo" dataIndex="priceCost" key="priceCost" render={(value) => formatterNumber(value)} />
+        <Column title="Preço de Venda" dataIndex="priceSell" key="priceSell" render={(value) => formatterNumber(value)} />
+        <Column title="Estoque" dataIndex="quantity" key="quantity" />
         <Column title="Medida" dataIndex="measurement" key="measurement" />
-        <Column title="Sob Demanda" dataIndex="onDemand" key="onDemand" />
         <Column
-          title="Action"
+          title="Sob Demanda"
+          dataIndex="isOnDemand"
+          key="isOnDemand"
+          render={(value) => (value ? <CheckOutlined /> : <CloseOutlined />)}
+        />
+        <Column
+          title="Ações"
           key="action"
-          render={() => (
+          render={(value) => (
             <Space size="middle">
-              <Link to="/">Alterar</Link>
-              <Link to="/">Excluir</Link>
+              <Link to={`/product/update/${value.code}`}>Alterar</Link>
+              <Link to={`/product/delete/${value.code}`}>Excluir</Link>
             </Space>
           )}
         />
       </Table>
+    );
+  }
+
+  return (
+    <div style={{ padding: 32 }}>
+      <PageHeader title="Produtos" extra={getButtonNewProduct()} />
+      { products && renderTable() }
     </div>
   );
 }
