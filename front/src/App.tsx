8e56e52093f229ed0e428bf1a16db28ca05653f3
WillianMendes <willianmendesbr@gmail.com>
2021-08-27 18:41:19 -0300

    refactor: routes

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -3,45 +3,44 @@ import {
   BrowserRouter, Route, Switch,
 } from 'react-router-dom';
 
-import { Layout } from 'antd';
-
 import 'antd/dist/antd.css';
 import './styles/global.css';
 
 import ProtectedRoute from './components/ProtectedRoute';
-import MainMenu from './components/MainMenu';
 import Dashboard from './pages/Dashboard';
 import Login from './pages/Login';
 import RecoveryPassword from './pages/RecoveryPassword';
 import Products from './pages/Products';
 import ProductRegister from './pages/ProductRegister';
+import NotFound from './pages/NotFound';
+import LayoutPanel from './components/LayoutPanel';
 
 function App() {
-  const { Content } = Layout;
   return (
     <BrowserRouter>
+      <LayoutPanel>
+        <Switch>
+          <ProtectedRoute path="/dashboard">
+            <Dashboard />
+          </ProtectedRoute>
+          <ProtectedRoute path="/product" exact>
+            <Products />
+          </ProtectedRoute>
+          <ProtectedRoute path="/product/update/:code">
+            <ProductRegister />
+          </ProtectedRoute>
+          <ProtectedRoute path="/product/register">
+            <ProductRegister />
+          </ProtectedRoute>
+          <ProtectedRoute path="*">
+            <Route component={NotFound} />
+          </ProtectedRoute>
+        </Switch>
+      </LayoutPanel>
       <Switch>
         <Route path="/login" component={Login} />
         <Route path="/recovery" component={RecoveryPassword} />
-        <Layout style={{ minHeight: '100vh' }}>
-          <MainMenu />
-          <Layout>
-            <Content>
-              <ProtectedRoute path="/dashboard">
-                <Dashboard />
-              </ProtectedRoute>
-              <ProtectedRoute path="/product" exact>
-                <Products />
-              </ProtectedRoute>
-              <ProtectedRoute path="/product/update/:code">
-                <ProductRegister />
-              </ProtectedRoute>
-              <ProtectedRoute path="/product/register">
-                <ProductRegister />
-              </ProtectedRoute>
-            </Content>
-          </Layout>
-        </Layout>
+        <Route component={NotFound} />
       </Switch>
     </BrowserRouter>
   );
