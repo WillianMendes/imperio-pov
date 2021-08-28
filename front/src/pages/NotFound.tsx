1b907c1284e5e8f1ffb369023e3849d65229ccb2
WillianMendes <willianmendesbr@gmail.com>
2021-08-28 16:33:43 -0300

    feat: not found link to back home

--- front/src/pages/NotFound.tsx
+++ front/src/pages/NotFound.tsx
@@ -1,14 +1,25 @@
-import { Button, Result } from 'antd';
 import React from 'react';
+import { Link } from 'react-router-dom';
+
+import { Button, Result } from 'antd';
+
+import { ADMIN_URL_APP_DASHBOARD } from '../const/ROUTES_ADMIN';
 
 function NotFound() {
+  function renderButtonBackHome() {
+    return (
+      <Link to={ADMIN_URL_APP_DASHBOARD}>
+        <Button type="primary">Back Home</Button>
+      </Link>
+    );
+  }
   return (
     <div style={{ padding: 32 }}>
       <Result
         status="404"
         title="404"
         subTitle="Desculpe, essa página não existe ou o servidor está fora do ar."
-        extra={<Button type="primary">Back Home</Button>}
+        extra={renderButtonBackHome()}
       />
     </div>
   );
